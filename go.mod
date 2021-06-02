1b2c163b5003700f318f6f2548bfe9fe51ee9083
Nuno Morais <nm.morais@campus.fct.unl.pt>
2021-06-02 13:47:30 +0100

    add yaml requirement, add main.go

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/nm-morais/go-babel v1.0.2
 	github.com/sirupsen/logrus v1.8.1
+	gopkg.in/yaml.v2 v2.4.0
 )
 
 replace github.com/nm-morais/go-babel => ../go-babel
---
+++ main.go
@@ -0,0 +1,161 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/nm-morais/Cyclon_T-Man/protocol"
+	babel "github.com/nm-morais/go-babel/pkg"
+	"github.com/nm-morais/go-babel/pkg/peer"
+	"gopkg.in/yaml.v2"
+)
+
+var (
+	randomPort          *bool
+	randomAnalyticsPort *bool
+	bootstraps          *string
+	listenIP            *string
+	confFilePath        *string
+)
+
+func main() {
+	for i := 0; i < len(os.Args); i++ {
+		fmt.Printf("arg %d: %s\n", i, os.Args[i])
+	}
+
+	randomPort = flag.Bool("rport", false, "choose random port")
+	randomAnalyticsPort = flag.Bool("raport", false, "choose random analytics port")
+	confFilePath = flag.String("conf", "config/exampleConfig.yml", "specify conf file path")
+	bootstraps = flag.String("bootstraps", "", "choose custom bootstrap nodes (space-separated ip:port list)")
+	listenIP = flag.String("listenIP", "", "choose custom ip to listen to")
+
+	flag.Parse()
+
+	conf := readConfFile(*confFilePath)
+
+	if *randomPort {
+		fmt.Println("Setting custom port")
+		freePort, err := GetFreePort()
+		if err != nil {
+			panic(err)
+		}
+		conf.SelfPeer.Port = freePort
+	}
+	if *randomAnalyticsPort {
+		fmt.Println("Setting custom analytics port")
+		freePort, err := GetFreePort()
+		if err != nil {
+			panic(err)
+		}
+		conf.SelfPeer.AnalyticsPort = freePort
+	}
+
+	ParseBootstrapArg(bootstraps, conf)
+	if listenIP != nil && *listenIP != "" {
+		fmt.Printf("Have custom ip: %s ", *listenIP)
+		conf.SelfPeer.Host = *listenIP
+	} else {
+		fmt.Printf("Do not have custom ip: %s ", *listenIP)
+	}
+
+	conf.LogFolder += fmt.Sprintf("%s:%d/", conf.SelfPeer.Host, conf.SelfPeer.Port)
+	selfPeer := peer.NewPeer(net.ParseIP(conf.SelfPeer.Host), uint16(conf.SelfPeer.Port), uint16(conf.SelfPeer.AnalyticsPort))
+
+	protoManagerConf := babel.Config{
+		Silent:    false,
+		LogFolder: conf.LogFolder,
+		SmConf: babel.StreamManagerConf{
+			BatchMaxSizeBytes: 2000,
+			BatchTimeout:      time.Second,
+			DialTimeout:       time.Millisecond * time.Duration(conf.DialTimeoutMiliseconds),
+		},
+		HandshakeTimeout: 8 * time.Second,
+		Peer:             selfPeer,
+	}
+
+	p := babel.NewProtoManager(protoManagerConf)
+	p.RegisterListenAddr(&net.TCPAddr{IP: protoManagerConf.Peer.IP(), Port: int(protoManagerConf.Peer.ProtosPort())})
+	p.RegisterListenAddr(&net.UDPAddr{IP: protoManagerConf.Peer.IP(), Port: int(protoManagerConf.Peer.ProtosPort())})
+	p.RegisterProtocol(protocol.NewCyclonProtocol(p, conf))
+	p.StartSync()
+}
+
+func readConfFile(path string) *protocol.CyclonConfig {
+	f, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	cfg := &protocol.CyclonConfig{}
+	decoder := yaml.NewDecoder(f)
+	err = decoder.Decode(cfg)
+	if err != nil {
+		panic(err)
+	}
+	return cfg
+}
+
+func GetFreePort() (port int, err error) {
+	var a *net.TCPAddr
+	if a, err = net.ResolveTCPAddr("tcp", "localhost:0"); err == nil {
+		var l *net.TCPListener
+		if l, err = net.ListenTCP("tcp", a); err == nil {
+			defer l.Close()
+			return l.Addr().(*net.TCPAddr).Port, nil
+		}
+	}
+	return
+}
+
+func ParseBootstrapArg(arg *string, conf *protocol.CyclonConfig) {
+	if arg != nil && *arg != "" {
+		bootstrapPeers := []struct {
+			Port          int    `yaml:"port"`
+			Host          string `yaml:"host"`
+			AnalyticsPort int    `yaml:"analyticsPort"`
+		}{}
+		fmt.Println("Setting custom bootstrap nodes")
+		for _, ipPortStr := range strings.Split(*arg, " ") {
+			split := strings.Split(ipPortStr, ":")
+			ip := split[0]
+
+			// assume all peers are running in same port if port is not specified
+			var portInt int = conf.SelfPeer.Port
+			var analyticsPortInt int = conf.SelfPeer.AnalyticsPort
+
+			if len(split) > 1 {
+				portIntAux, err := strconv.ParseInt(split[1], 10, 32)
+				if err != nil {
+					panic(err)
+				}
+				portInt = int(portIntAux)
+			}
+
+			if len(split) > 2 {
+				portIntAux, err := strconv.ParseInt(split[2], 10, 32)
+				if err != nil {
+					panic(err)
+				}
+				analyticsPortInt = int(portIntAux)
+			}
+
+			fmt.Println(ip)
+			bootstrapPeers = append(bootstrapPeers, struct {
+				Port          int    `yaml:"port"`
+				Host          string `yaml:"host"`
+				AnalyticsPort int    `yaml:"analyticsPort"`
+			}{
+				Port:          portInt,
+				Host:          ip,
+				AnalyticsPort: analyticsPortInt,
+			})
+		}
+		conf.BootstrapPeers = bootstrapPeers
+	}
+}
